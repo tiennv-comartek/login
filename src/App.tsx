d6bec43c7a2f7e90244977551e56f64d27e14d9e
tiennv-comartek <tiennv@comartek.com>
2022-10-15 14:10:09 +0700

    update hooks login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import "antd/dist/antd.min.css";
+import { Suspense,  } from "react";
+import { RouterProvider, } from "react-router-dom";
+
 import "./assets/scss/app.scss";
-import Login from "./modules/auth/pages/Login";
+import { browserRouters, } from "./routes";
+
+
+
 
 function App() {
+
+  
   return (
-    <>
-      <Login />
-    </>
-  );
+    <Suspense fallback={<div>Loading...</div>}>
+    <RouterProvider router={browserRouters}/>
+  </Suspense>
+  )
 }
 
 export default App;
---
+++ src/modules/auth/hooks/useLogin.ts
@@ -0,0 +1,32 @@
+import { notification } from 'antd';
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { systemRoutes } from '../../../routes';
+
+const useLogin = () => {
+    const navigate = useNavigate();
+    const onLogin = useCallback((value: any): any => {
+        if ((value.username === "ngocxinxu@gmail.com" && value.password === '123') || (value.username === "karik@gmail.com" && value.password === 'lll')) {
+            notification.open({
+                message: 'Đăng nhập thành công...'
+            })
+            navigate(systemRoutes.HOME_PAGE);
+        }
+        else {
+            notification.open({
+                message: 'Tên tài khoản hoặc mật khẩu không đúng!'
+            })
+         
+    
+            navigate(systemRoutes.LOGIN_ROUTE);
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    
+    return {
+        onLogin
+    }
+};
+
+
+export default useLogin;
